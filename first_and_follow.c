a57c911626bfa4f9dca6e485ce4222f63fdaa1df
itsarthurmorgan <140511073+itsarthurmorgan@users.noreply.github.com>
2024-02-01 13:02:02 +0530

    Update first_and_follow.c

--- first_and_follow.c
+++ first_and_follow.c
@@ -1,56 +1,113 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+#include<malloc.h>
+#include<stdio.h>
+#include<string.h>
 
-#define MAX_RULES 10
-#define MAX_SYMBOLS 10
+char **productions;
+int fvar;
 
-char nonterminals[MAX_RULES];
-char firstsets[MAX_RULES][MAX_SYMBOLS];
-char followsets[MAX_RULES][MAX_SYMBOLS];
+int findPos(char NonTer)
+{
+	int i=0;
+	while(productions[i][0]!=NonTer)
+		i++;
+	return i;
+}
+char* findGenerating(char Ter)
+{
+	int i=0;
+	while(productions[i][0]!=Ter)
+		i++;
+	return productions[i];
+}
+char findFirst(char *prod)
+{
+	int i,j=0;
+	for(i=3;i<strlen(prod);i++)
+	{	
+		if(prod[i]>=97 && prod[i]<=122 || prod[i]==')' || prod[i]=='(' || prod[i]==',')
+		{	
+			printf(" %c ",prod[i]);
 
-void computefirstset(char nonterminal) {
-    // Implementation of computefirstset
+			while(prod[i]!='/' && prod[i]!='\0')
+				i++;
+		}
+		else if(prod[i]>=65 && prod[i]<=90)
+		{
+			printf("  %c", findFirst(productions[findPos(prod[i])]));
+			return;
+		}
+		else if(prod[i]=='#')
+		{
+			printf("  #");
+		}
+		else 
+			continue;
+	}
+	return;
 }
 
-void computefollowset(char nonterminal) {
-    // Implementation of computefollowset
+void findFollow(char GeneratingSymbol,int n)
+{
+	int i,j=0;
+	if(GeneratingSymbol=='S')
+			printf(" $ ");
+	for(j=0;j<n;j++)
+	{
+		for(i=3;i<strlen(productions[j]);i++)
+		{	
+			if(GeneratingSymbol==productions[j][i])
+			{
+				if(productions[j][i+1] >= 97 && productions[j][i+1] <= 122 || productions[j][i+1]==')' || productions[j][i+1]=='(' || productions[j][i+1]==',')
+				{	
+					printf(" %c ",productions[j][i+1]);
+				}
+				else if(productions[j][i+1] >= 65 && productions[j][i+1] <= 90)
+				{
+					char ans=findFirst(findGenerating(productions[j][i+1]));
+				}
+				else if(i+1==strlen(productions[j]))
+				{
+					findFollow(productions[j][0],n);
+				}
+				else 
+					continue;
+			}
+		}
+	}
 }
 
-int main() {
-    int numRules;
-    printf("Enter the number of production rules: ");
-    scanf("%d", &numRules);
-
-    // Clear newline from the input buffer
-    while (getchar() != '\n');
-
-    for (int i = 0; i < numRules; i++) {
-        printf("Enter production rule %d (format: nonterminal -> production): ", i + 1);
-        scanf(" %c->%s", &nonterminals[i], firstsets[i]);
-    }
-
-    for (int i = 0; i < numRules; i++) {
-        memset(followsets[i], 0, sizeof(followsets[i]));
-    }
-
-    for (int i = 0; i < numRules; i++) {
-        computefirstset(nonterminals[i]);
-        computefollowset(nonterminals[i]);
-    }
-
-    for (int i = 0; i < numRules; i++) {
-        printf("First(%c)={%s}\n", nonterminals[i], firstsets[i]);
-        printf("Follow(%c)={%s}\n", nonterminals[i], followsets[i]);
-    }
-
-    return 0;
+int main()
+{
+	int j,i,n,k,tempInd,flag=0;
+	printf("Enter the number of productions");
+	scanf("%d\n",&n);
+	productions=(char**)malloc(sizeof(char*)*n);
+	for(i=0;i<n;i++)
+		productions[i]=(char*)malloc(sizeof(char)*20);
+	
+	char **productionsDup=(char**)malloc(sizeof(char*)*n);
+	for(i=0;i<n;i++)
+		productionsDup[i]=(char*)malloc(sizeof(char)*20);
+	
+	for(i=0;i<n;i++)
+	{
+		gets(productions[i]);
+		productionsDup[i]=productions[i];
+	}
+	
+	//First Computation
+	for(i=0;i<n;i++)
+	{
+		printf("\nFIRST(%c)={  ",productions[i][0]);
+		char ans=findFirst(productions[i]);
+		printf("}\n");
+	}
+	
+	for(fvar=0;fvar<n;fvar++)
+	{
+		printf("\nFOLLOW(%c)={",productions[fvar][0]);
+		findFollow(productions[fvar][0],n);
+		printf("}\n");
+	}
+	printf("\nThe End");
 }
-
-//o:Enter the number of production rules: 2
-// Enter production rule 1 (format: nonterminal -> production): S->AB
-// Enter production rule 2 (format: nonterminal -> production): A->a
-// First(S)={AB}
-// Follow(S)={}
-// First(A)={a}
-// Follow(A)={}
