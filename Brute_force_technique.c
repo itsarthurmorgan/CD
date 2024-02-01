cb924bd3ae8c9e50266f0f520fc348b47953c618
itsarthurmorgan <140511073+itsarthurmorgan@users.noreply.github.com>
2024-02-01 22:11:17 +0530

    Update Brute_force_technique.c

--- Brute_force_technique.c
+++ Brute_force_technique.c
@@ -1,103 +1,97 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-void check(void);
-void set_value_backtracking(void);
-void get_value_backtracking(void);
-void display_output_string(void);
-int iptr=0,optr=0,current_optr=0;
-char output_string[20],current_output_string[20],input_string[20],temp_string[20];
-int main(){
-printf("\nEnter the string to check: ");
-scanf("%s",input_string);
-check();
-return 0;
-}
-void check(void){
-int flag=1,rule2_index=1;
-strcpy(output_string,"S");
-printf("\nThe output string in different stages are:\n");
-while(iptr<=strlen(input_string)){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-if(strcmp(output_string,temp_string)!=0){
+#define MAX_RULES 10
+#define MAX_SYMBOLS 10
 
-display_output_string();
+typedef struct {
+    char nonTerminal;
+    char rhs[MAX_RULES][MAX_SYMBOLS];
+    int numRules;
+} ProductionRule;
 
+ProductionRule rules[MAX_RULES];
+int numRules;
+
+int match(char c, char **input) {
+    if (**input == c) {
+        (*input)++;
+        return 1;
+    }
+    return 0;
 }
 
-if((iptr!=strlen(input_string)) || (optr!=strlen(output_string))){
-if(input_string[iptr]==output_string[optr]){
-iptr=iptr+1;
-optr=optr+1;
-}
-else{
-if(output_string[optr]=='S'){
-memset(output_string,0,strlen(output_string));
-strcpy(output_string,"cAd");
-}
-else if(output_string[optr]=='A'){
-
-set_value_backtracking();
-if(rule2_index==1){
-memset(output_string,0,strlen(output_string));
-strcpy(output_string,"cabd");
+void displayRules() {
+    printf("\nGrammar Rules:\n");
+    for (int i = 0; i < numRules; i++) {
+        printf("%c -> %s", rules[i].nonTerminal, rules[i].rhs[0]);
+        for (int j = 1; j < rules[i].numRules; j++) {
+            printf("\n| %s", rules[i].rhs[j]);
+        }
+        printf("\n");
+    }
 }
 
-else{
-memset(output_string,0,strlen(output_string));
-strcpy(output_string,"cad");
-}
-}
-else if(output_string[optr]=='b' && input_string[iptr]=='d'){
+int parse(char *input, char nonTerminal) {
+    for (int i = 0; i < numRules; i++) {
+        if (rules[i].nonTerminal == nonTerminal) {
+            for (int j = 0; j < rules[i].numRules; j++) {
+                char *originalInput = input;
+                int success = 1;
 
-rule2_index=2;
-get_value_backtracking();
-iptr=iptr-1;
+                for (int k = 0; k < strlen(rules[i].rhs[j]); k++) {
+                    if (!match(rules[i].rhs[j][k], &input)) {
+                        success = 0;
+                        break;
+                    }
+                }
 
-}
+                if (success) {
+                    printf("Successfully matched rule: %c -> %s\n", nonTerminal, rules[i].rhs[j]);
+                    return 1;
+                }
 
-else{
-printf("\nThe given string, '%s' is invalid.\n\n",input_string);
-break;
+                input = originalInput;
+            }
+        }
+    }
 
-}
-}
-}
-
-else{
-
-printf("\nThe given string, '%s' is valid.\n\n",input_string);
-break;
-}
-}
-}
-void set_value_backtracking(void){
-//setting values for backtracking
-current_optr=optr;
-strcpy(current_output_string,output_string);
-return;
-}
-void get_value_backtracking(void){
-//backtracking and obtaining previous values
-optr=current_optr;
-memset(output_string,0,strlen(output_string));
-strcpy(output_string,current_output_string);
-return;
-}
-void display_output_string(void){
-printf("%s\n",output_string);
-memset(temp_string,0,strlen(temp_string));
-strcpy(temp_string,output_string);
-return;
+    return 0;
 }
 
-//o:  Enter the string to check: cad
+int main() {
+    printf("Enter the number of production rules: ");
+    scanf("%d", &numRules);
 
-// The output string in different stages are:
-// S
-// cAd
-// cabd
-// cAd
-// cad
+    printf("Enter the production rules:\n");
+    for (int i = 0; i < numRules; i++) {
+        printf("Rule %d (Non-terminal): ", i + 1);
+        scanf(" %c", &rules[i].nonTerminal);
 
-// The given string, 'cad' is valid.
+        printf("Number of rules for %c: ", rules[i].nonTerminal);
+        scanf("%d", &rules[i].numRules);
+
+        printf("Enter the rules (separated by spaces): ");
+        for (int j = 0; j < rules[i].numRules; j++) {
+            scanf("%s", rules[i].rhs[j]);
+        }
+    }
+
+    displayRules();
+
+    char input[100];
+    printf("Enter the input string: ");
+    scanf("%s", input);
+
+    char *inputPtr = input;
+    int success = parse(inputPtr, rules[0].nonTerminal);
+
+    if (success) {
+        printf("Parsing successful!\n");
+    } else {
+        printf("Parsing failed!\n");
+    }
+
+    return 0;
+}
